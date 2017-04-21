622e2fece8dfc12e07beb71312e03bd4712ed974
Michael Klein <miketheklein@gmail.com>
2017-04-21 14:03:09 -0700

    Adds landing.js and tests | Moves globalState logic to landing from App

---
+++ source/components/App/Landing.js
@@ -0,0 +1,31 @@
+/* global __HOST__ */
+
+import React, { Component } from 'react'
+import axios from 'axios'
+import ProjectMenuContainer from '../ProjectMenu/ProjectMenuContainer'
+import LoginContainer from '../Login/LoginContainer'
+import componentErrorHandler from '../utilities/componentErrorHandler'
+
+export default class Landing extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentDidMount() {
+    axios.get( `${__HOST__}/session` )
+      .then( ({ data: { userId } }) => {
+        if ( userId ) { this.setState({ userId }) }
+      })
+      .catch( componentErrorHandler( 'App' ) )
+  }
+
+  render() {
+    const userId = this.state.userId
+    const loginOrProjectMenu = userId ?
+      <ProjectMenuContainer userId={ userId } /> :
+      <LoginContainer />
+
+    return loginOrProjectMenu
+  }
+}
---
+++ source/components/App/_tests/Landing.test.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import sinon from 'sinon'
+import moxios from 'moxios'
+import { mount } from 'enzyme'
+import { expect } from 'sym/configuration/testSetup'
+import Landing from '../Landing'
+
+describe( '<Landing />', () => {
+
+  context( 'handles data from HTTP request on componentDidMount', () => {
+    let wrapper, mountSpy
+    const fakeData = { userId: 9000 }
+
+    beforeEach( () => {
+      moxios.install()
+      mountSpy = sinon.spy( Landing.prototype, 'componentDidMount' )
+      wrapper = mount( <Landing /> )
+    })
+
+    afterEach( () => {
+      moxios.uninstall()
+      mountSpy.restore()
+      wrapper.unmount()
+    })
+
+    it( 'calls componentDidMount ', () => {
+      expect( Landing.prototype.componentDidMount.calledOnce ).to.equal( true )
+    })
+
+    it( 'sets intial state using data from HTTP response', done =>
+      moxios.wait( () => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: fakeData
+        }).then( () => {
+          expect( wrapper.state().userId ).to.eql( 9000 )
+          done()
+        }).catch( done )
+      })
+    )
+
+    it( 'renders <ProjectMenuContainer />', done =>
+      moxios.wait( () => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: fakeData
+        }).then( () => {
+          expect( wrapper.find( 'ProjectMenuContainer' ).length ).to.equal( 1 )
+          done()
+        }).catch( done )
+      })
+    )
+
+  })
+
+  context( 'handles error returned from HTTP request on componentDidMount', () => {
+    let errorStub, wrapper
+
+    beforeEach( () => {
+      moxios.install()
+      errorStub = sinon.stub( console, 'warn' ).callsFake( () => null )
+      wrapper = mount( <Landing /> )
+    })
+
+    afterEach( () => {
+      errorStub.restore()
+      moxios.uninstall()
+      wrapper.unmount()
+    })
+
+    it( 'it catches and responds with an error', done =>
+      moxios.wait( () => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 400,
+          response: 'fakeError'
+        }).then( () => {
+          expect( errorStub.calledTwice ).to.equal( true )
+          done()
+        }).catch( done )
+      })
+    )
+
+    it( 'renders <LoginContainer />', done =>
+      moxios.wait( () => {
+        const request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 400,
+          response: 'fakeError'
+        }).then( () => {
+          expect( wrapper.find( 'LoginContainer' ).length ).to.equal( 1 )
+          done()
+        }).catch( done )
+      })
+    )
+
+  })
+
+})
