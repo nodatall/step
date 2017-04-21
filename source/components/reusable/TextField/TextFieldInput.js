1f87d63a0eb511e0aceee7645dd597c0f49d5366
Michael Klein <miketheklein@gmail.com>
2017-04-21 13:58:09 -0700

    Return truncate tables in setup.js

--- source/components/reusable/TextField/TextFieldInput.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-const TextFieldInput = ({ value, onChange, onKeyUp }) =>
-  <input onChange={ onChange } value={ value } onKeyUp={ onKeyUp } />
-
-export default TextFieldInput
