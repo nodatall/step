f3b96590882ac1b469e3703f9eefe0c3a50d5506
Michael Klein <miketheklein@gmail.com>
2017-04-21 14:12:32 -0700

    Adds mock couldDos and projects to mockComponentData

--- source/testUtilities/mockComponentData.js
+++ source/testUtilities/mockComponentData.js
@@ -1,27 +1,84 @@
-const mockGlobalState = {
-  currentProjectId: 2,
-  couldDos: {
-    1: [
-      { text: 'make my breakfast', id: 1 },
-      { text: 'eat my breakfast', id: 2 },
-      { text: 'relax', id: 3 }
-    ],
-    2: [
-      { text: 'pick some flowers', id: 4 },
-      { text: 'eat some flowers', id: 5 },
-      { text: 'smell some flowers', id: 6 }
-    ],
-    3: [
-      { text: 'call a friend', id: 7 },
-      { text: 'give unsolicited advice to people', id: 8 },
-      { text: 'make a painting', id: 9 }
-    ],
+const mockGlobalStateCouldDos = {
+  1: {
+    id: 1,
+    text: 'give unsolicited advice to people',
   },
-  projects: [
-    { text: 'hello', id: 1 },
-    { text: 'goodbye', id: 2 }
-  ],
+  2: {
+    id: 2,
+    text: 'make a painting',
+  },
+  3: {
+    id: 3,
+    text: 'sculpt creature from super sculpy',
+  },
+}
+
+const mockGlobalStateProjects = {
+  1: {
+    id: 1,
+    text: 'learn to chortle',
+    couldDos: mockGlobalStateCouldDos
+  },
+  2: {
+    id: 2,
+    text: 'learn to be creative while fearing for your livelihood',
+    couldDos: mockGlobalStateCouldDos
+  },
+  3: {
+    id: 3,
+    text: 'enter the danger zone',
+    couldDos: mockGlobalStateCouldDos
+  },
+  4: {
+    id: 4,
+    text: 'break into someone\'s house and organize thier stuff',
+    couldDos: mockGlobalStateCouldDos
+  }
+}
+
+const mockGlobalState = {
+  userId: 1,
+  currentProjectId: 2,
+  projects: mockGlobalStateProjects,
   currentCouldDoIndex: 0
 }
 
-export default mockGlobalState
+const mockProjects = [
+  {
+    id: 1,
+    text: 'learn to chortle',
+    couldDos: mockGlobalStateCouldDos
+  },
+  {
+    id: 2,
+    text: 'learn to be creative while fearing for your livelihood',
+    couldDos: mockGlobalStateCouldDos
+  },
+  {
+    id: 3,
+    text: 'enter the danger zone',
+    couldDos: mockGlobalStateCouldDos
+  },
+  {
+    id: 4,
+    text: 'break into someone\'s house and organize thier stuff',
+    couldDos: mockGlobalStateCouldDos
+  }
+]
+
+const mockCouldDos = [
+  {
+    id: 1,
+    text: 'give unsolicited advice to people',
+  },
+  {
+    id: 2,
+    text: 'make a painting',
+  },
+  {
+    id: 3,
+    text: 'sculpt creature from super sculpy',
+  },
+]
+
+export { mockGlobalState, mockProjects, mockCouldDos }
---
+++ source/testUtilities/mockDatabaseData.js
@@ -0,0 +1,108 @@
+const mockCouldDoData = {
+  fakeCouldDo1: {
+    id: 998,
+    text: 'eat breakfast',
+    user_id: 1,
+    project_id: 1
+  },
+  fakeCouldDo2: {
+    id: 977,
+    text: 'eat zero sugar',
+    user_id: 1,
+    project_id: 1
+  },
+  fakeCouldDo3: {
+    id: 456,
+    text: 'be excellent to each other',
+    user_id: 1,
+    project_id: 7
+  },
+  invalidCouldDo: {
+    text: 'make toast',
+    how_dark: 'burnt'
+  },
+  fakeEdit: {
+    text: 'eat lunch'
+  }
+}
+
+const mockProjectData = {
+  fakeProject1: {
+    id: 77,
+    text: 'eating',
+    user_id: 1,
+  },
+  fakeProject2: {
+    id: 88,
+    text: 'sleeping',
+    user_id: 1,
+  },
+  fakeProject3: {
+    id: 99,
+    text: 'dreaming',
+    user_id: 2,
+  },
+  invalidProject: {
+    color: 'chartreuse',
+    existance: 'meaningless',
+  },
+  fakeEdit: {
+    text: 'snoozing',
+  }
+}
+
+const mockUserData = {
+  fakeUser1: {
+    id: 77,
+    email: 'john@hallman.com',
+    oauth_ID: 123456789,
+    created_at: '2017-04-03 15:46:05.965-07',
+    updated_at: null,
+    display_name: 'John',
+    refresh_token: 'a1b2c3d4e5',
+  },
+  fakeUser2: {
+    id: 88,
+    email: 'steve@weber.com',
+    oauth_ID: 987654321,
+    created_at: '2017-04-04 15:46:05.965-07',
+    updated_at: null,
+    display_name: 'Steve',
+    refresh_token: 'a1b2c3d4e5f6g7h8',
+  },
+  fakeUser3: {
+    id: 99,
+    email: 'sylvan@nodatall.com',
+    oauth_ID: 999999999,
+    created_at: '2017-04-01 15:46:05.965-07',
+    updated_at: null,
+    display_name: 'Sylvan',
+    refresh_token: 'a111111111b',
+  },
+  invalidUser: {
+    home_address: 'Oakland',
+    name: 'Jennifer'
+  },
+  fakeEdit: {
+    display_name: 'Batman'
+  },
+  fakeGoogleProfile: {
+    oauth_ID: '1234',
+    email: 'cows@mom.com',
+    display_name: 'Dialate',
+    refresh_token: '123565'
+  },
+  googleResponse: {
+    request: {},
+    accesToken: 123,
+    refresh_token: 1234,
+    profile: {
+      id: 1,
+      email: 'earth@universe.org',
+      name: { givenName: 'emptiness' }
+    }
+  }
+}
+
+
+export { mockCouldDoData, mockProjectData, mockUserData }
