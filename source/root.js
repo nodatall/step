ef283ca77ad8b07a9be276e1d0cab23669059ddf
Sylvan Swierkosz <sswierkosz@gmail.com>
2018-01-09 17:53:55 -0800

    navigates to / on any page reload

--- source/root.js
+++ source/root.js
@@ -2,4 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App/App'
 
+if ( performance.navigation.type === 1 ) {
+  location.replace( '/' )
+}
+
 render( <App />, document.getElementById( 'anchor' ) )
