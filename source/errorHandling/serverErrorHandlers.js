a78226aa2368fd40385d26ab8c7a38831b7c859d
Sylvan Swierkosz <sswierkosz@gmail.com>
2018-01-23 14:39:24 -0800

    updates server error handler

--- source/errorHandling/serverErrorHandlers.js
+++ source/errorHandling/serverErrorHandlers.js
@@ -13,20 +13,25 @@ const handleUnhandledRejection = () => {
 const handleServerErrors = ( error, request, response, next ) => { // eslint-disable-line
   if ( !process.env.NODE_ENV === 'test' ) handleUnhandledRejection()
   
-  logger.error( `\n${chalk.yellow( error.createMessage() )}` )
-  
-  response.status( error.status || 500 )
-  
-  if ( process.env.NODE_ENV === 'development' ) {
-    response.json({
-      message: error.createMessage(),
-      stack: error.stack
-    })
+  if ( typeof error.createMessage === 'undefined' ) {
+    console.log('error', error)
   } else {
-    response.json({
-      message: error.createMessage()
-    })
+    logger.error( `\n${chalk.yellow( error.createMessage() )}` )
+    
+    response.status( error.status || 500 )
+    
+    if ( process.env.NODE_ENV === 'development' ) {
+      response.json({
+        message: error.createMessage(),
+        stack: error.stack
+      })
+    } else {
+      response.json({
+        message: error.createMessage()
+      })
+    }
   }
+  
 }
 
 const handleDatabaseError = ( error, message ) => {
